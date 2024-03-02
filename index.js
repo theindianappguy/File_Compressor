a9427823554dd9570e7ebe78ecd9e47d87a43e34
Pratiksha Kanoja <70995557+Pratiksha-Kanoja@users.noreply.github.com>
2024-03-02 20:05:17 +0530

    new

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 const path = require('path');
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.Port || 3000;
 
 app.use(express.json());
 app.use(fileUpload());
